f67ce57aea96a2d1b4589e20168082c4f0f7b1f1
Temmarie <temmar_whyte16@yahoo.com>
2020-07-26 10:19:58 +0100

    made friendships mutual

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def destroy_duplicates
+    inverse = Friendship.find_by(user_id: friend, friend_id: user)
+    destroy
+    inverse.destroy
+  end
 end
