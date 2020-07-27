ebf01007dc5a16b3cbb2236f8c056bb278430734
kotoku kennedy <kennedy.kotoku@gmail.com>
2020-07-27 17:16:58 +0200

    Revert "Mile stone 6: friendship v2"

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,4 @@
 class FriendshipsController < ApplicationController
-  def index
-    @friendship = Friendship.all
-  end
-
   def send_request
     if current_user.send_invitation(params[:user_id])
       flash.notice = 'Friend request sent'
@@ -37,59 +33,15 @@ class FriendshipsController < ApplicationController
     inverse_friend&.delete
   end
 
-  # def destroy
-  #   user = User.find(params[:user_id])
-  #   @relationship = current_user.friendships.find_by_friend_id(user)
-  #   if @relationship.present?
-  #     @relationship.destroy
-  #   end
-  #   redirect_to users_path
-  # end
-
-  # def destroy
-  #   @relationship = Friendship.find(params[:id])
-  #   @relationship.destroy
-  #   redirect_to users_path
-  # end
-
-  # def destroy
-  #   user = User.find(params[:user_id])
-  #   friend_request = user.friendships.find_by_friend_id(user.id)
-
-  #   friendship = current_user.friendships.find_by_friend_id(user.id)
-  #   if friendship.nil?
-  #     friend_request.destroy
-  #     flash[:notice] = "Removed friend."
-  #   else
-  #     friendship.destroy
-  #     friend_request.destroy
-  #     flash[:notice] = "Removed friendship."
-  #   end
-  #   redirect_to users_path
-  # end
-
-  # def destroy
-  #   user = User.find(params[:user_id])
-  #   friend = current_user.friendships.find_by_friend_id(user)
-  #   if friend
-  #     friend.delete
-  #     flash.notice = "#{user.name} has been removed as your friend"
-  #     redirect_to users_path
-  #   else
-  #     flash.now[:notice] = 'error occured'
-  #   end
-  # end
-
   def destroy
     user = User.find(params[:user_id])
     friend = current_user.friendships.find_by_friend_id(user)
-    friend.destroy
-    redirect_to user_path(id: current_user.id)
-  end
 
-  private
-
-  def friendship_params
-    params.permit(:user_id, :friend_id)
+    if friend.delete
+      flash.notice = "#{user.name} has been removed as your friend"
+      redirect_to users_path
+    else
+      flash.now[:notice] = 'error occurred'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
   root 'posts#index'
-
+  delete 'remove_friend' => 'friendships#destroy'
   get 'send_request' => 'friendships#send_request'
   get 'accept_invite' => 'friendships#accept_invitation'
   get 'pending_request' => 'friendships#pending_invitation'
   delete 'reject_invite' => 'friendships#reject_invitation'
-  delete 'remove_friend' => 'friendships#destroy'
 
 
 
   devise_for :users
-  resources :friendships
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
